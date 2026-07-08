61737a81f4e3376b5ec71c14c337929caa6d60d9
Azhan Ali <azhanali2304@gmail.com>
2026-07-08 13:31:17 +0530

    Time: 9 ms (7.16%) | Memory: 9.8 MB (25.73%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n = s.size();
-        int tsz = t.size();
-        int count = 0;
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
+        if(s==t) return true;
+        else return false;
+        
+        // int n = s.size();
+        // int tsz = t.size();
+        // int count = 0;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
 
-        if(n==tsz){
-            for(int i=0;i<n;i++){
-            if(s[i] == t[i]) count++;
-        }
-            if(count==n) return true;
-            else{
-                return false;
-            }
-        } else{
-            return false;
-        }
+        // if(n==tsz){
+        //     for(int i=0;i<n;i++){
+        //     if(s[i] == t[i]) count++;
+        // }
+        //     if(count==n) return true;
+        //     else{
+        //         return false;
+        //     }
+        // } else{
+        //     return false;
+        // }
         
     }
 };
