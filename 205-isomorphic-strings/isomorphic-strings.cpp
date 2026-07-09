27227a3043627fcaba88740fd83deb74c946f472
Azhan Ali <azhanali2304@gmail.com>
2026-07-09 22:24:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (94.69%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        vector<int> v(150,1000);
+        // string s ka point of view sa 
+        for(int i=0;i<s.size();i++){
+            int idx = (int)s[i];
+            if(v[idx]==1000) v[idx] = s[i] - t[i];
+            else if(v[idx]!=(s[i] -t[i])) return false;
+        }
+
+        // emptying the vector
+        for(int i=0;i<150;i++){
+            v[i]=1000;
+        }
+
+        //  string t ka point of view sa
+        for(int i=0;i<s.size();i++){
+            int idx = (int)t[i];
+            if(v[idx]==1000) v[idx] = t[i] - s[i];
+            else if(v[idx]!=(t[i] -s[i])) return false;
+        }
+
+        return true;
+    }
+};
