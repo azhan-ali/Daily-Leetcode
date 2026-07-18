68dc98a8540f0676d906ffa7990e03f00b99733a
Azhan Ali <azhanali2304@gmail.com>
2026-07-18 20:46:06 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (8.64%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,7 +1,32 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
-        return ;
+        int n = nums.size();
+        // Find the breakpoint
+        int idx = -1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx = i;
+                break;
+            }
+        }
+        // Edge Case [5,4,3,2,1]
+        if(idx==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+
+        // Find the smallest element greater than nums[idx]
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>nums[idx]){
+                swap(nums[i],nums[idx]);
+                break;
+            }
+        }
+
+        // Reverse the suffix 
+        reverse(nums.begin() + idx + 1, nums.end());
+
+        return;
     }
 };
